c203a1a48ddafecf0f4a301e472bbbea68c73f41
dindaa06 <adindaaekatrisna@gmail.com>
2024-12-06 17:01:46 +0800

    Push code to remote repository

---
+++ src/server/handler.js
@@ -0,0 +1,11 @@
+/*Kode disembunyikan*/
+const storeData = require('../services/storeData');
+ 
+async function postPredictHandler(id, data) {
+ 
+  /*Kode disembunyikan*/
+  await storeData(id, data);
+ 
+  /*Kode disembunyikan*/
+}
+/*Kode disembunyikan*/
